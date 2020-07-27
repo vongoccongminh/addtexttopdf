45d29f1972ca53e3d6b98038fcbdd939fec4c72a
Cong Minh Vo Ngoc <congminhvo171@gmail.com>
2020-07-27 18:24:19 +0700

    finish

--- src/com/minh/Main.java
+++ src/com/minh/Main.java
@@ -1,14 +1,10 @@
 package com.minh;
 
-import org.apache.pdfbox.multipdf.LayerUtility;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.pdmodel.PDPageContentStream;
 import org.apache.pdfbox.pdmodel.encryption.InvalidPasswordException;
 import org.apache.pdfbox.pdmodel.font.PDType0Font;
-import org.apache.pdfbox.pdmodel.font.PDType1Font;
-import org.apache.pdfbox.pdmodel.graphics.color.PDColor;
-import org.apache.pdfbox.pdmodel.graphics.state.RenderingMode;
 
 import java.awt.*;
 import java.io.File;
@@ -18,13 +14,18 @@ import java.util.List;
 
 public class Main {
 
+    public static final int fontSizeName = 36;
+
+    public static final int fontSizeDistance = 16;
+
     public static void main(String[] args) throws IOException {
 
         List<Data> dataList = new ArrayList<>();
 
-        dataList.add(new Data("Nguyễn Văn A", "25 km"));
-        dataList.add(new Data("TRƯƠNG TRẦN NGÔ", "30 km"));
-        dataList.add(new Data("Usain Bolt", "100 m"));
+        dataList.add(new Data("Nguyễn Văn A", "25km"));
+        dataList.add(new Data("TRƯƠNG TRẦN NGÔ", "30km"));
+        dataList.add(new Data("Usain Bolt", "100m"));
+        dataList.add(new Data("Đỗ Nguyễn Lê Phùng Võ Phạm Bùi", "5km"));
 
         dataList.forEach(data -> {
             try {
@@ -34,38 +35,32 @@ public class Main {
                 //Creating a PDF Document
                 PDPage page = doc.getPage(0);
 
-//                System.out.println("height : " + page.getMediaBox().getHeight());
-
-//                System.out.println("with : " + page.getMediaBox().getWidth());
-
-
                 String dir = "liberation-sans/LiberationSans-Bold.ttf";
                 PDType0Font font = PDType0Font.load(doc, new File(dir));
 
                 PDPageContentStream contentStream = new PDPageContentStream(doc, page, PDPageContentStream.AppendMode.APPEND, false, true);
 
-                contentStream.setNonStrokingColor(Color.RED);
-
                 //Begin the Content stream
                 contentStream.beginText();
-
+                contentStream.setNonStrokingColor(Color.RED);
                 //Setting the font to the Content stream
-                contentStream.setFont(font, 36);
-
+                contentStream.setFont(font, fontSizeName);
+                //Get text name width
+                float textNameWidth = font.getStringWidth(data.getName()) / 1000 * fontSizeName;
                 //Setting the position for the line
-                contentStream.newLineAtOffset(270, 210);
-
+                contentStream.newLineAtOffset(350 - textNameWidth / 2, 210);
                 //Adding text in the form of string
                 contentStream.showText(data.getName());
-
-//                contentStream.newLine();
-//                contentStream.setFont(font, 36);
-//                contentStream.setNonStrokingColor(Color.BLUE);
-//                contentStream.newLineAtOffset(0, 0);
-//                contentStream.showText(data.getDistance());
-
                 //Ending the content stream
                 contentStream.endText();
+
+                contentStream.beginText();
+                contentStream.setFont(font, fontSizeDistance);
+                contentStream.setNonStrokingColor(Color.BLUE);
+                contentStream.newLineAtOffset(392, 157);
+                contentStream.showText(data.getDistance());
+                contentStream.endText();
+
                 System.out.println("Content added");
 
                 //Closing the content stream
@@ -83,7 +78,5 @@ public class Main {
                 e.printStackTrace();
             }
         });
-
-
     }
 }
