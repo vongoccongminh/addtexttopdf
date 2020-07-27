5f690fc0115fbfb45821a53f60cbec52147882eb
Cong Minh Vo Ngoc <congminhvo171@gmail.com>
2020-07-28 00:24:48 +0700

    add

--- src/com/minh/Main.java
+++ src/com/minh/Main.java
@@ -1,17 +1,17 @@
 package com.minh;
 
-import org.apache.pdfbox.pdmodel.PDDocument;
-import org.apache.pdfbox.pdmodel.PDDocumentCatalog;
-import org.apache.pdfbox.pdmodel.PDPage;
-import org.apache.pdfbox.pdmodel.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.*;
 import org.apache.pdfbox.pdmodel.common.PDRectangle;
 import org.apache.pdfbox.pdmodel.encryption.InvalidPasswordException;
+import org.apache.pdfbox.pdmodel.font.PDFont;
 import org.apache.pdfbox.pdmodel.font.PDType0Font;
 import org.apache.pdfbox.pdmodel.interactive.form.PDAcroForm;
 import org.apache.pdfbox.pdmodel.interactive.form.PDField;
+import org.apache.pdfbox.pdmodel.interactive.form.PDTextField;
 
 import java.awt.*;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -26,14 +26,14 @@ public class Main {
 
         List<Data> dataList = new ArrayList<>();
 
-        dataList.add(new Data("Nguyễn Văn A", "25km"));
-        dataList.add(new Data("TRƯƠNG TRẦN NGÔ", "30km"));
-        dataList.add(new Data("Usain Bolt", "100m"));
+        dataList.add(new Data("Lê Bảo Toàn", "100km"));
+        dataList.add(new Data("Lê Bảo Toàn", "42km"));
+//        dataList.add(new Data("Usain Bolt", "100m"));
 
         dataList.forEach(data -> {
             try {
                 //Loading an existing document
-                PDDocument doc = PDDocument.load(new File("OoPdfFormExample.pdf"));
+                PDDocument doc = PDDocument.load(new File("e-certificate_RMS.pdf"));
 
                 //Creating a PDF Document
                 PDPage page = doc.getPage(0);
@@ -41,13 +41,26 @@ public class Main {
                 String dir = "liberation-sans/LiberationSans-Bold.ttf";
                 PDType0Font font = PDType0Font.load(doc, new File(dir));
 
+                String dirArial = "arial.ttf";
+
                 PDDocumentCatalog documentCatalog = doc.getDocumentCatalog();
                 PDAcroForm acroForm = documentCatalog.getAcroForm();
                 List<PDField> fields = acroForm.getFields();
 
+                PDFont formFont = PDType0Font.load(doc, new FileInputStream(dirArial), false); // check that the font has what you need; ARIALUNI.TTF is good but huge
+                PDResources res = acroForm.getDefaultResources(); // could be null, if so, then create it with the setter
+                String fontName = res.add(formFont).getName();
+                String defaultAppearanceString = "/" + fontName + " 0 Tf 0 g"; // adjust to replace existing font name
+
                 for (PDField field : fields) {
-                    insert(field, doc, page, "Given Name Text Box", font, data.getName());
-                    insert(field, doc, page, "Family Name Text Box", font, data.getDistance());
+                    PDTextField textField = (PDTextField) field;
+                    textField.setDefaultAppearance(defaultAppearanceString);
+
+                    textField.setValue(data.getName());
+                    textField.setReadOnly(true);
+
+//                    insert(field, doc, page, "fullName",  font, data.getName());
+//                    insert(field, doc, page, "distance",  font, data.getDistance());
                 }
 
                 //Saving the document
